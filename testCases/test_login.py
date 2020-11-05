e1ee2e82fa3faa23f0df44ef97d440f6ee00f38e
nikhilraju <tumiki.nikhil@gmail>
2020-11-05 09:28:08 +0530

    first commit

---
+++ testCases/test_login.py
@@ -0,0 +1,55 @@
+import pytest
+from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+
+class Test_001_Login:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+    logger=LogGen.loggen()
+
+    @pytest.mark.regression
+    def test_homePageTitle(self,setup):
+        self.driver = setup
+        #self.driver=webdriver.Chrome()
+        #driver.Chrome(execute path)
+        #maxmize window
+        #minimize window
+        self.driver.get(self.baseURL)
+        act_title=self.driver.title
+        if act_title=="Your store. Login":
+            self.driver.close()
+            assert True
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
+            self.driver.close()
+            assert False
+
+    @pytest.mark.sanity
+    @pytest.mark.regression
+    def test_login(self,setup):
+
+        self.logger.info("****Started Login Test****")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp=LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title=self.driver.title
+        if act_title=="Dashboard / nopCommerce administration":
+            self.logger.info("****Login test passed ****")
+            self.driver.close()
+            assert True
+        else:
+            self.logger.error("****Login test failed ****")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_homePageTitle.png")
+            self.driver.close()
+            assert False
+
+
+
+
+
